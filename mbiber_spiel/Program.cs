b78565dacdc64efb28a5038e31fbac220beaead4
Matteo Biber <mbiber@schueler.hakzell.at>
2020-03-24 12:33:39 +0100

    git ignore/projekt erstöt

---
+++ mbiber_spiel/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mbiber_spiel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
